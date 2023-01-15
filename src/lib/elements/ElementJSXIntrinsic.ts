c802bc41a92e67480f2251516faf4aea6d8a8859
Mark Chris Levy <levy@codeandmore.pl>
2023-01-15 19:43:07 +0100

    commit

--- src/lib/elements/ElementJSXIntrinsic.ts
+++ src/lib/elements/ElementJSXIntrinsic.ts
@@ -4,5 +4,6 @@ import {CustomElement} from "./CustomElement";
 import {ElementEventsProps} from "./ElementEventsProps";
 import {ElementProps} from "./ElementProps";
 
+type Kebab<T extends string, A extends string = ""> = T extends `${infer F}${infer R}` ? Kebab<R, `${A}${F extends Lowercase<F> ? "" : "-"}${Lowercase<F>}`> : A;
 export type ElementJSXIntrinsic<Element extends CustomElement, Events extends {[P in keyof Events]: Event} = any> =
     KebabCasedProperties<ElementProps<Element>> & JSX.HTMLAttributes<Element> & ElementEventsProps<Element, Events>;
