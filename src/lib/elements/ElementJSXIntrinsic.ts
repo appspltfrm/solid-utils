5bf709c790c186bc02c7c332ef4779ba3f4d01f8
Mark Chris Levy <levy@codeandmore.pl>
2023-01-15 16:55:24 +0100

    commit

--- src/lib/elements/ElementJSXIntrinsic.ts
+++ src/lib/elements/ElementJSXIntrinsic.ts
@@ -1,7 +1,8 @@
 import {JSX} from "solid-js/h/jsx-runtime";
 import {KebabCasedProperties} from "type-fest";
+import {CustomElement} from "./CustomElement";
 import {ElementEventsProps} from "./ElementEventsProps";
 import {ElementProps} from "./ElementProps";
 
-export type ElementJSXIntrinsic<Element extends HTMLElement, Events extends {[P in keyof Events]: Event} = any> =
+export type ElementJSXIntrinsic<Element extends CustomElement, Events extends {[P in keyof Events]: Event} = any> =
     KebabCasedProperties<ElementProps<Element>> & JSX.HTMLAttributes<Element> & ElementEventsProps<Element, Events>;
--- src/lib/elements/ElementTemplate.ts
+++ src/lib/elements/ElementTemplate.ts
@@ -1,4 +1,5 @@
 import {JSX} from "solid-js/h/jsx-runtime";
+import {CustomElement} from "./CustomElement";
 import {ElementProps} from "./ElementProps";
 
-export type ElementTemplate<Element extends HTMLElement> = {props: ElementProps<Element>, children?: JSX.Element};
+export type ElementTemplate<Element extends CustomElement> = {props: ElementProps<Element>, children?: JSX.Element};
