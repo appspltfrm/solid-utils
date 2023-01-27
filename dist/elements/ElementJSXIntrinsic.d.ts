66228afcb7b63861ced4b4a2068ab44569a93c80
Mark Chris Levy <levy@codeandmore.pl>
2023-01-27 15:13:11 +0100

    commit

--- dist/elements/ElementJSXIntrinsic.d.ts
+++ dist/elements/ElementJSXIntrinsic.d.ts
@@ -1,8 +1,9 @@
 import { JSX } from "solid-js/h/jsx-runtime";
 import { CustomElement } from "./CustomElement";
 import { ElementEventsProps } from "./ElementEventsProps";
+import { ElementProps } from "./ElementProps";
 type KebabCasePropName<T extends string, A extends string = ""> = T extends `${infer F}${infer R}` ? KebabCasePropName<R, `${A}${F extends Lowercase<F> ? "" : "-"}${Lowercase<F>}`> : A;
-export type ElementJSXIntrinsic<Element extends CustomElement, Props, Events extends {
+export type ElementJSXIntrinsic<Element extends CustomElement, Props = ElementProps<Element>, Events extends {
     [P in keyof Events]: Event;
 } = any> = {
     [P in keyof Props as (P extends string ? KebabCasePropName<P> : never)]: Props[P];
--- src/lib/elements/ElementJSXIntrinsic.ts
+++ src/lib/elements/ElementJSXIntrinsic.ts
@@ -1,10 +1,11 @@
 import {JSX} from "solid-js/h/jsx-runtime";
 import {CustomElement} from "./CustomElement";
 import {ElementEventsProps} from "./ElementEventsProps";
+import {ElementProps} from "./ElementProps";
 
 type KebabCasePropName<T extends string, A extends string = ""> = T extends `${infer F}${infer R}` ? KebabCasePropName<R, `${A}${F extends Lowercase<F> ? "" : "-"}${Lowercase<F>}`> : A;
 // export type ElementJSXIntrinsic<Element extends CustomElement, Props, Events extends {[P in keyof Events]: Event} = any> =
 //     KebabCasedProperties<Props> & JSX.HTMLAttributes<Element> & ElementEventsProps<Element, Events>;
 
-export type ElementJSXIntrinsic<Element extends CustomElement, Props, Events extends {[P in keyof Events]: Event} = any> =
+export type ElementJSXIntrinsic<Element extends CustomElement, Props = ElementProps<Element>, Events extends {[P in keyof Events]: Event} = any> =
     {[P in keyof Props as (P extends string ? KebabCasePropName<P> : never)]: Props[P]} & JSX.HTMLAttributes<Element> & ElementEventsProps<Element, Events>;
